fd7b86be05fbdf575ec25471409c03bb391cba41
JoYongHyeon <whdydgus5778@gmail.com>
2025-10-27 09:35:04 +0900

    first commit

---
+++ src/main/java/tech/gate/step/springbatchpractice/SpringBatchPracticeApplication.java
@@ -0,0 +1,13 @@
+package tech.gate.step.springbatchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchPracticeApplication.class, args);
+    }
+
+}
